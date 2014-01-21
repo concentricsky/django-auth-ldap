280b38cf5fb04c8645fb2c2987f7273e0c1496df
krimkus <krimkus@gmail.com>
2014-01-21 15:07:16 -0800

    Update models.py
    Replaced foreignkey to User with settings.AUTH_USER_MODEL

--- django_auth_ldap/models.py
+++ django_auth_ldap/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class TestProfile(models.Model):
@@ -6,6 +7,6 @@ class TestProfile(models.Model):
     A user profile model for use by unit tests. This has nothing to do with the
     authentication backend itself.
     """
-    user = models.OneToOneField('auth.User')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     is_special = models.BooleanField(default=False)
     populated = models.BooleanField(default=False)
